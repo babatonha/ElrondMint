ccc31eb0925726c28d12d146269838f96136f274
babatonha <40696889+babatonha@users.noreply.github.com>
2021-12-28 21:37:21 +0200

    clean the repository

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { renderWithRouter } from "testUtils";
-
-test("renders home page", () => {
-  const screen = renderWithRouter({ route: "/" });
-  const title = screen.getByTestId("title");
-  expect(title).toBeInTheDocument();
-});
